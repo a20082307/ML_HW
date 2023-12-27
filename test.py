7ac8e08c74e4bb3eded39026f741fefe5bc76ddb
gmyhp <rogerline0527@gmail.com>
2023-12-28 01:57:21 +0800

    Almost finish Basic part

--- test.py
+++ test.py
@@ -1,12 +1,9 @@
 import numpy as np
-print(5 + 6 + 8 + 7 + 9 + 8 + 10 + 11)
-print(2 + 4 + 4 + 6 + 6 + 7 + 9 + 10)
-
-a = np.array([
-    [-3, -2, 0, -1, 1, 0, 2, 3],
-    [-4, -2, -2, 0, 0, 1, 3, 4]
+test = np.array([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
 ])
-print(np.dot(a, a.T) / 8)
 
-print(np.dot(np.array([0.59, 0.81]), np.array([-0.81, 0.59])))
-print(float(9.34 / (9.34 + 0.41)))
+print(test[:2].T)
+print(test[ : , : 2])
